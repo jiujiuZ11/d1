dc4741572611f29991f9a3d6979e5974f1ca459d
Joe Politz <joe.politz@gmail.com>
2023-11-09 12:18:49 -0800

    Sorter

---
+++ submissions/student5/Sorter.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Sorter {
+  public static void main(String[] args) {
+    ArrayList<Integer> a = new ArrayList<>();
+    Scanner in = new Scanner(System.in);
+
+    String s = in.nextLine();
+
+    for(String num: s.split(" ")) {
+      a.add(Integer.parseInt(num));
+    }
+    // sort the arraylist a in place
+    // Filled by copilot
+    for(int i = 0; i < a.size(); i += 1) {
+        for(int j = 0; j < a.size() - 1; j += 1) {
+            if(a.get(j) > a.get(j + 1)) {
+            int temp = a.get(j);
+            a.set(j, a.get(j + 1));
+            a.set(j + 1, temp);
+            }
+        }
+    }
+
+
+    for(Integer i: a) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+  }
+}
+
